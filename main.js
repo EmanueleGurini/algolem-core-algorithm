2b906445ceb2aa8a26955381c024cfe83f78c9f4
egurini <egurini@teclasystem.com>
2021-11-02 22:04:09 +0100

    Fix files

--- main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var data_1 = require("./data/data");
-var multi_element_filter_algorithm_1 = require("./multi-element-filter-algorithm");
-//getWorkersWithCategories(workers, workersPreset);
-console.table(multi_element_filter_algorithm_1.getWorkersWithCategories(data_1.workers, data_1.workersPreset));
