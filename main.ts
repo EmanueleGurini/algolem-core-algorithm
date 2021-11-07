749886da4c9fc6ee96f14eed6b79a8061aecae02
egurini <egurini@teclasystem.com>
2021-11-07 18:15:22 +0100

    First fix sinstax algorithm

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 import { workers, workersPreset } from './data/data'
 import { getWorkersWithCategories } from './multi-element-filter-algorithm'
 
-
-//getWorkersWithCategories(workers, workersPreset);
 console.table(getWorkersWithCategories(workers, workersPreset));
