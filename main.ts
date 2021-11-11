caa4f26a9841b236d796bb13496df422f9f1174e
egurini <egurini@teclasystem.com>
2021-11-11 09:37:13 +0100

    Fix Folder

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { workers, workersPreset } from './algolem/data'
-import { getWorkersWithCategoriesAndSubCategories } from './multi-element-filter-algorithm'
+import { getWorkersWithCategoriesAndSubCategories } from './algolem-algorithm'
 
-//console.table(getWorkersWithCategories(workers, workersPreset));
 console.log(getWorkersWithCategoriesAndSubCategories(workers, workersPreset));
